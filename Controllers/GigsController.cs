e7f7fda02aa7f6d3556063f947a09fcdd616dce1
Álvaro Gonçalves <alvaro.goncalves@cps.sp.gov.br>
2019-05-31 02:39:53 -0300

    Move DateTime calculation to GigFormViewModel.

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using DotNet_05.ViewModels;
 using DOtNet_05.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -9,11 +11,14 @@ namespace DotNet_05.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+        public DateTime DateTime { get; private set; }
+
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
         // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -21,6 +26,24 @@ namespace DotNet_05.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.DateTime,
+                GenreId  = viewModel.Genre,
+                Venue    = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- ViewModels/GigFormViewModel.cs
+++ ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DOtNet_05.Models;
+using System;
 using System.Collections.Generic;
 
 namespace DotNet_05.ViewModels
@@ -8,7 +9,8 @@ namespace DotNet_05.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime => DateTime.Parse(string.Format("{0} {1}", Date, Time));
     }
 }
