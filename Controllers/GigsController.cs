eee2beda1a3a3c84fb39336eb6d0adec0e1ec4ff
Álvaro Gonçalves <alvaro.goncalves@cps.sp.gov.br>
2019-05-31 00:16:30 -0300

    Create Add a Gig Form.

---
+++ Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using DotNet_05.ViewModels;
+using DOtNet_05.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DotNet_05.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DOtNet_05.Models;
+using System.Collections.Generic;
+
+namespace DotNet_05.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
